1ff60ceb6fec080f19e4b84f2939b50963e574ac
Serhiy Voytenko <serhiy.voytenko@dn.pebs.biz>
2021-05-20 12:32:16 +0300

    lesson 7 add different file menu

--- l07/arrays.php
+++ l07/arrays.php
@@ -1,7 +1,7 @@
 <?php
 $new = [1, 2, 3];
 $old = array(1, 2, 3);
-var_dump($new, $old);
+//var_dump($new, $old);
 
 $aert = [
     'name' => 'Serg',
@@ -31,143 +31,11 @@ $big = [
         ],
     ],
 ];
-var_dump($big);
+//var_dump($big);
 
-var_dump($aert);
+//var_dump($aert);
 unset($old[1]);
-var_dump($old);
+//var_dump($old);
 
 
-$array_menu = [
-    [
-        'id' => 10,
-        'parent' => 0,
-        'title' => 'Computers',
-        'childs' =>
-            [
-                [
-                    'id' => 11,
-                    'parent' => 10,
-                    'title' => 'Laptop',
-                    'childs' =>
-                        [
-                            [
-                                'id' => 12,
-                                'parent' => 11,
-                                'title' => 'Asus',
-                            ],
-                            [
-                                'id' => 13,
-                                'parent' => 11,
-                                'title' => 'Lenovo',
-                            ],
-                            [
-                                'id' => 14,
-                                'parent' => 11,
-                                'title' => 'Xiaomi',
-                            ],
-                            [
-                                'id' => 15,
-                                'parent' => 11,
-                                'title' => 'Apple'
-                            ],
-                        ],
-                ],
-                [
-                    'id' => 16,
-                    'parent' => 10,
-                    'title' => 'Tablets',
-                    'childs' =>
-                        [
-                            [
-                                'id' => 17,
-                                'parent' => 16,
-                                'title' => 'Xiaomi',
-                                'childs' =>
-                                    [
-                                        [
-                                            'id' => 18,
-                                            'parent' => 17,
-                                            'title' => 'White',
-                                        ],
-                                        [
-                                            'id' => 19,
-                                            'parent' => 17,
-                                            'title' => 'Black',
-                                        ],
-                                    ],
-                            ],
-                            [
-                                'id' => 20,
-                                'parent' => 16,
-                                'title' => 'Lenovo',
-                                'childs' =>
-                                    [
-                                        [
-                                            'id' => 21,
-                                            'parent' => 20,
-                                            'title' => 'White',
-                                        ],
-                                        [
-                                            'id' => 22,
-                                            'parent' => 20,
-                                            'title' => 'Black',
-                                        ],
-                                    ],
-                            ],
-                        ],
 
-                ],
-            ],
-    ],
-    [
-        'id' => 23,
-        'parent' => 0,
-        'title' => 'Clothes',
-        'childs' =>
-            [
-                [
-                    'id' => 24,
-                    'parent' => 23,
-                    'title' => 'Dresses',
-                ],
-                [
-                    'id' => 25,
-                    'parent' => 23,
-                    'title' => 'Chirts'
-                ],
-                [
-                    'id' => 26,
-                    'parent' => 23,
-                    'title' => 'Coats'
-                ],
-            ],
-    ],
-    [
-        'id' => 27,
-        'parent' => 0,
-        'title' => 'Sport',
-        'childs' =>
-            [
-                [
-                    'id' => 28,
-                    'parent' => 27,
-                    'title' => 'Boats'
-                ],
-                [
-                    'id' => 29,
-                    'parent' => 27,
-                    'title' => 'Bicycle',
-                ],
-            ],
-    ],
-    [
-        'id' => 30,
-        'parent' => 0,
-        'title' => 'About',
-    ],
-];
-
-echo '<pre>';
-print_r($array_menu);
-echo '</pre>';
---
+++ menu/menu.php
@@ -0,0 +1,190 @@
+<?php
+$array_menu = array(
+    array(
+        'id' => 10,
+        'parent' => 0,
+        'title' => 'Computers',
+        'link'=>'computers.php',
+        'childs' =>
+            array(
+                array(
+                    'id' => 11,
+                    'parent' => 10,
+                    'title' => 'Laptop',
+                    'link'=>'laptop.php',
+                    'childs' =>
+                        array(
+                            array(
+                                'id' => 12,
+                                'parent' => 11,
+                                'title' => 'Asus',
+                                'link'=>'asus.php',
+                            ),
+                            array(
+                                'id' => 13,
+                                'parent' => 11,
+                                'title' => 'Lenovo',
+                                'link'=>'lenovo.php',
+                            ),
+                            array(
+                                'id' => 14,
+                                'parent' => 11,
+                                'title' => 'Xiaomi',
+                                'link'=>'xiaomi.php',
+                            ),
+                            array(
+                                'id' => 15,
+                                'parent' => 11,
+                                'title' => 'Apple',
+                                'link'=>'apple.php',
+                            ),
+                        ),
+                ),
+                array(
+                    'id' => 16,
+                    'parent' => 10,
+                    'title' => 'Tablets',
+                    'link'=>'tablets.php',
+                    'childs' =>
+                        array(
+                            array(
+                                'id' => 17,
+                                'parent' => 16,
+                                'title' => 'Xiaomi',
+                                'link'=>'xiaomi.php',
+                                'childs' =>
+                                    array(
+                                        array(
+                                            'id' => 18,
+                                            'parent' => 17,
+                                            'title' => 'White',
+                                            'link'=>'white.php',
+                                        ),
+                                        array(
+                                            'id' => 19,
+                                            'parent' => 17,
+                                            'title' => 'Black',
+                                            'link'=>'black.php',
+                                        ),
+                                    ),
+                            ),
+                            array(
+                                'id' => 20,
+                                'parent' => 16,
+                                'title' => 'Lenovo',
+                                'link'=>'lenovo.php',
+                                'childs' =>
+                                    array(
+                                        array(
+                                            'id' => 21,
+                                            'parent' => 20,
+                                            'title' => 'White',
+                                            'link'=>'white.php',
+                                        ),
+                                        array(
+                                            'id' => 22,
+                                            'parent' => 20,
+                                            'title' => 'Black',
+                                            'link'=>'black.php',
+                                        ),
+                                    ),
+                            ),
+                        ),
+
+                ),
+            ),
+    ),
+    array(
+        'id' => 23,
+        'parent' => 0,
+        'title' => 'Clothes',
+        'childs' =>
+            array(
+                array(
+                    'id' => 24,
+                    'parent' => 23,
+                    'title' => 'Dresses',
+                    'link'=>'dresses.php',
+                ),
+                array(
+                    'id' => 25,
+                    'parent' => 23,
+                    'title' => 'Chirts',
+                    'link'=>'chirts.php',
+                ),
+                array(
+                    'id' => 26,
+                    'parent' => 23,
+                    'title' => 'Coats',
+                    'link'=>'coats.php',
+                ),
+            ),
+    ),
+    array(
+        'id' => 27,
+        'parent' => 0,
+        'title' => 'Sport',
+        'link'=>'sport.php',
+        'childs' =>
+            array(
+                array(
+                    'id' => 28,
+                    'parent' => 27,
+                    'title' => 'Boats',
+                    'link'=>'boats.php',
+                ),
+                array(
+                    'id' => 29,
+                    'parent' => 27,
+                    'title' => 'Bicycle',
+                    'link'=>'bicycle.php',
+                ),
+            ),
+    ),
+    array(
+        'id' => 30,
+        'parent' => 0,
+        'title' => 'About',
+        'link'=>'about.php',
+    ),
+);
+
+//echo '<pre>';
+//print_r($array_menu);
+//echo '</pre>';
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<nav>
+    <p>Collapsing:</p>
+    <ul class="collapsable">
+        <li>a<ul>
+                <li>a1
+                <li>a2
+            </ul>
+        <li>b<ul>
+                <li>b1
+            </ul>
+    </ul>
+    <p>Not collapsing:</p>
+    <ul>
+        <li>a<ul>
+                <li>a1
+                <li>a2
+            </ul>
+        <li>b<ul>
+                <li>b1
+            </ul>
+    </ul>
+</nav>
+</body>
+</html>
+
