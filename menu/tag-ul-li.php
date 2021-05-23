95910e4ba72dfd7eb661457bbeb8e6d4d1222ed4
Serhiy Voytenko <serhiy.voytenko@dn.pebs.biz>
2021-05-23 20:39:00 +0300

    lesson 8 full lesson recursive build menu

--- menu/tag-ul-li.php
+++ menu/tag-ul-li.php
@@ -1,5 +1,160 @@
 <?php
 
+$array_menu = [
+    [
+        'id' => 10,
+        'parent' => 0,
+        'title' => 'Computers',
+        'link' => 'computers.php',
+        'children' =>
+           [
+                [
+                    'id' => 11,
+                    'parent' => 10,
+                    'title' => 'Laptop',
+                    'link' => 'laptop.php',
+                    'children' =>
+                        [
+                            [
+                                'id' => 12,
+                                'parent' => 11,
+                                'title' => 'Asus',
+                                'link' => 'asus.php',
+                            ],
+                            [
+                                'id' => 13,
+                                'parent' => 11,
+                                'title' => 'Lenovo',
+                                'link' => 'lenovo.php',
+                            ],
+                            [
+                                'id' => 14,
+                                'parent' => 11,
+                                'title' => 'Xiaomi',
+                                'link' => 'xiaomi.php',
+                            ],
+                            [
+                                'id' => 15,
+                                'parent' => 11,
+                                'title' => 'Apple',
+                                'link' => 'apple.php',
+                            ],
+                        ],
+                ],
+                [
+                    'id' => 16,
+                    'parent' => 10,
+                    'title' => 'Tablets',
+                    'link' => 'tablets.php',
+                    'children' =>
+                        [
+                            [
+                                'id' => 17,
+                                'parent' => 16,
+                                'title' => 'Xiaomi',
+                                'link' => 'xiaomi.php',
+                                'children' =>
+                                    [
+                                        [
+                                            'id' => 18,
+                                            'parent' => 17,
+                                            'title' => 'White',
+                                            'link' => 'white.php',
+                                        ],
+                                        [
+                                            'id' => 19,
+                                            'parent' => 17,
+                                            'title' => 'Black',
+                                            'link' => 'black.php',
+                                        ],
+                                    ],
+                            ],
+                            [
+                                'id' => 20,
+                                'parent' => 16,
+                                'title' => 'Lenovo',
+                                'link' => 'lenovo.php',
+                                'children' =>
+                                    [
+                                        [
+                                            'id' => 21,
+                                            'parent' => 20,
+                                            'title' => 'White',
+                                            'link' => 'white.php',
+                                        ],
+                                        [
+                                            'id' => 22,
+                                            'parent' => 20,
+                                            'title' => 'Black',
+                                            'link' => 'black.php',
+                                        ],
+                                    ],
+                            ],
+                        ],
+
+                ],
+            ],
+    ],
+    [
+        'id' => 23,
+        'parent' => 0,
+        'link' => 'dresses.php',
+        'title' => 'Clothes',
+        'children' =>
+            [
+                [
+                    'id' => 24,
+                    'parent' => 23,
+                    'title' => 'Dresses',
+                    'link' => 'dresses.php',
+                ],
+                [
+                    'id' => 25,
+                    'parent' => 23,
+                    'title' => 'Chirts',
+                    'link' => 'chirts.php',
+                ],
+                [
+                    'id' => 26,
+                    'parent' => 23,
+                    'title' => 'Coats',
+                    'link' => 'coats.php',
+                ],
+            ],
+    ],
+    [
+        'id' => 27,
+        'parent' => 0,
+        'title' => 'Sport',
+        'link' => 'sport.php',
+        'children' =>
+            [
+                [
+                    'id' => 28,
+                    'parent' => 27,
+                    'title' => 'Boats',
+                    'link' => 'boats.php',
+                ],
+                [
+                    'id' => 29,
+                    'parent' => 27,
+                    'title' => 'Bicycle',
+                    'link' => 'bicycle.php',
+                ],
+            ],
+    ],
+    [
+        'id' => 30,
+        'parent' => 0,
+        'title' => 'About',
+        'link' => 'about.php',
+        'children'=>[
+            'title'=>'Email US',
+            'link'=>'email.php',
+        ],
+    ],
+];
+
 $data = [
     ['title' => 'TITLE', 'link' => '/link'],
     ['title' => 'TITLE2', 'link' => '/link2'],
@@ -61,5 +216,5 @@ function recursive_link($menu)
     }
 }
 
-recursive_link($data);
+recursive_link($array_menu);
 
