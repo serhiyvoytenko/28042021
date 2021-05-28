ec85e4bce6a620de64ce2f4c6d1ce165d055a885
Serhiy Voytenko <serhiy.voytenko@dn.pebs.biz>
2021-05-28 09:10:06 +0300

    lesson 9 simple array for workhome

--- l08/homework.php
+++ l08/homework.php
@@ -3,7 +3,7 @@
 $data = [
     ['title' => 'TITLE', 'link' => '/link'],
     ['title' => 'TITLE2', 'link' => '/link2'],
-    ['title' => 'PARENT 1', 'link' => '/link3', 'children' => [
+    ['title' => 'PARENT 1', 'children' => [
         ['title' => 'CHILD', 'link' => '/child-link'],
         ['title' => 'CHILD2', 'link' => '/child-link2'],
     ]],
@@ -11,10 +11,18 @@ $data = [
 /*
  * Your PHP - code here
  */
-$html='';
-foreach ($data as $item){
-    $html .="<li><a href='{$item['link']}'>{$item['title']}</a></li>";
+$html = '<ul>';
+foreach ($data as $value) {
+    if (!array_key_exists('children', $value)) {
+        $html .= "<li><a href='{$value['link']}'>{$value['title']}</a></li>";
+    }else{
+        $html .="<li>{$value['title']}<ul>";
+        foreach ($value['children'] as $child){
+            $html .= "<li><a href='{$child['link']}'>{$child['title']}</a></li>";
+        }$html.="</ul></li>";
+    }
 }
+$html .= '</ul>';
 echo $html;
 //$html = <<<HTML
 //<ul>
