afe83c1c4cbedb801e94f357feff8d3d1b43edb3
Serhiy Voytenko <serhiy.voytenko@dn.pebs.biz>
2021-06-04 16:30:58 +0300

    lesson 11 home forms gitignore

--- l11/home/text-handler.php
+++ l11/home/text-handler.php
@@ -1,3 +1,15 @@
 <?php
-header('Location: forms.php');
+
+declare(strict_types=1);
+
+$comment = serialize($_POST);
+
+$dir = __DIR__ . '/folder/' . date('Y-m-d');
+if (!is_dir($dir)) {
+    mkdir($dir);
+}
+
+$file = $dir.'/'.time().'_'.md5($comment).'.txt';
+file_put_contents($file,$comment);
+    header('Location: forms.php');
 //var_dump($_POST);
