c1ec1a0d14c179ddbb46c9d8f5c058fa2ba08f47
Serhiy Voytenko <serhiy.voytenko@dn.pebs.biz>
2021-06-05 21:46:16 +0300

    lesson 11 home comments-list.php

--- l11/home/title.php
+++ l11/home/title.php
@@ -1,17 +1,26 @@
 <?php
 require_once __DIR__ . '/header.php';
-$comment = require __DIR__.'/comment-list.php';
+$comment = require __DIR__ . '/comment-list.php';
+//var_dump($comment);
 ?>
 
 <?php
 require_once __DIR__ . '/form.php';
 ?>
     <div class="container-fluid pb-3">
-    <div class="bg-light border rounded-3 p-3">
-        <?php
-        var_dump($comment);
-        ?>
-    </div>
+        <div class="bg-light border rounded-3 p-3">
+            <?php foreach ($comment as $day => $value): ?>
+                <?php foreach ($value as $time => $values): ?>
+                    <b>Создано: <?= $day.' '.$time ?></b><br>
+                    <b>Отредактировано: <?= $day.' '.$time ?></b><br>
+                    Пользователь: <?= $values['my-name'] ?><br>
+                    Пол: <?= $values['gender'] ?><br>
+                    Язык программирования: <?= $values['language'] ?><br>
+                    Описание:<br><?= $values['text'] ?>
+                    <hr>
+                <?php endforeach; ?>
+            <?php endforeach; ?>
+        </div>
     </div>
 
 <?php
