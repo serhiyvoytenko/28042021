d030d459039a9cb7d17cced90f31c3ae49cd997f
Serhiy Voytenko <serhiy.voytenko@dn.pebs.biz>
2021-06-10 09:09:03 +0300

    lesson 13 home work

--- l13/breadcrumbs.php
+++ l13/breadcrumbs.php
@@ -12,7 +12,6 @@ $fullPath = '?rout=';
 <nav aria-label="breadcrumb">
     <ol class="breadcrumb">
         <li class="breadcrumb-item active" aria-current="page">
-            <!--            --><? //= $rout ?: '/' ?>
             <?php foreach ($routeArray as $link): ?>
                 <?php $fullPath .= '/'.$link; ?>
                 /<a href="<?= $fullPath ?>"><?= $link ?></a>
